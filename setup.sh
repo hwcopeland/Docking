a3abf7ae658c8647345810bc95e835c13427ecec
hwcopeland <hwc2e@mtmail.mtsu.edu>
2023-11-11 20:38:32 +0000

    modified:   setup.sh 	Ligands/

--- setup.sh
+++ setup.sh
@@ -1,11 +1,30 @@
 #!/bin/bash
 
-# Install conda
-wget https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh
-bash Miniconda3-latest-Linux-x86_64.sh -b -p $HOME/miniconda
+# Check the operating system
+OS="$(uname)"
+MACHINE="$(uname -m)"
 
-# Add conda to PATH
-export PATH="$HOME/miniconda/bin:$PATH"
+if [ "$OS" == "Linux" ]; then
+    # Install conda
+    wget https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh
+    bash Miniconda3-latest-Linux-x86_64.sh -b -p $HOME/miniconda
+
+    # Add conda to PATH
+    export PATH="$HOME/miniconda/bin:$PATH"
+elif [ "$OS" == "Darwin" ]; then
+    if [ "$MACHINE" == "arm64" ]; then
+        # Install conda for ARM
+        wget https://github.com/conda-forge/miniforge/releases/latest/download/Miniforge3-MacOSX-arm64.sh
+        bash Miniforge3-MacOSX-arm64.sh -b -p $HOME/miniconda
+    else
+        # Install conda for Intel
+        wget https://repo.anaconda.com/miniconda/Miniconda3-latest-MacOSX-x86_64.sh
+        bash Miniconda3-latest-MacOSX-x86_64.sh -b -p $HOME/miniconda
+    fi
+
+    # Add conda to PATH
+    export PATH="$HOME/miniconda/bin:$PATH"
+fi
 
 # Create a new conda environment
 conda create -n docking python=3.7
