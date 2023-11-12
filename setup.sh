b4af9f77b72b8ca0b48f38c276aa0009282deed0
hwcopeland <hwc2e@mtmail.mtsu.edu>
2023-11-12 04:40:50 +0000

    modified:   setup.sh

--- setup.sh
+++ setup.sh
@@ -33,12 +33,13 @@ conda create -n docking python=3.7
 source activate docking
 
 # Install conda packages
-conda install -c conda-forge rdkit
 conda install -c conda-forge fpocket
-conda install -c conda-forge vina
+conda install -c bioconda vina
 conda install -c schrodinger pymol
 conda install -c conda-forge biopython
 conda install -c conda-forge openbabel
+
+pip install rdkit
 
 export PATH="$HOME/miniconda/envs/docking/bin:$PATH"
 export PATH="$HOME/miniconda/bin:$PATH"0
